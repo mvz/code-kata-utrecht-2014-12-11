ab0fbfd955d0ef2d506cdbe7e0bf829f3599d040
Matijs van Zuijlen <matijs@matijs.net>
2014-11-11 12:45:12 +0100

    Implement rule 'rectangular grid'
    Notice what's missing here. What are the hidden assumptions about the
    rectangular grid?

--- lib/life.rb
+++ lib/life.rb
@@ -1,1 +1,15 @@
 # Your code goes here
+
+class Grid
+  def initialize x: nil, y: nil
+
+  end
+
+  def cell_at x:, y:
+    Cell.new
+  end
+end
+
+class Cell
+
+end
--- test/life_test.rb
+++ test/life_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 require 'life'
 
 class LifeTest < MiniTest::Test
-  def test_hello
-    pass
+  def test_rectangular_grid
+    grid = Grid.new(x: 4, y: 3)
+    assert_instance_of Cell, grid.cell_at(x: 2, y: 3)
   end
 end
